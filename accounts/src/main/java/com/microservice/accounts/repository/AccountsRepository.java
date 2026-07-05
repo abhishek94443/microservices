542257c027a8bcfb0c2fac7395ecb720016e374c
abhishek94443 <abhishekdwivedi9444@gmail.com>
2026-07-06 02:34:02 +0530

    Added ResourceNotfoundException and crud api endpoints for Account Service

--- accounts/src/main/java/com/microservice/accounts/repository/AccountsRepository.java
+++ accounts/src/main/java/com/microservice/accounts/repository/AccountsRepository.java
@@ -2,9 +2,18 @@ package com.microservice.accounts.repository;
 
 import com.microservice.accounts.entity.Accounts;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Repository
 public interface AccountsRepository extends JpaRepository< Accounts,Long> {
-    
+
+    void deleteByCustomerId(Long customerId);
+
+    @Transactional
+    @Modifying
+    Optional<Accounts> findByCustomerId(Long customerId);
 }
