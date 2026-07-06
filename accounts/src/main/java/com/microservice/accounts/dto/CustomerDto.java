44e24c072ffbcf9ce0675a89d2a25a5595cb62b9
abhishek94443 <abhishekdwivedi9444@gmail.com>
2026-07-06 23:50:20 +0530

    Global adding exception

--- accounts/src/main/java/com/microservice/accounts/dto/CustomerDto.java
+++ accounts/src/main/java/com/microservice/accounts/dto/CustomerDto.java
@@ -3,16 +3,26 @@ package com.microservice.accounts.dto;
 
 
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 
 public class CustomerDto {
 
+   @Size(min =5, max=30,message = "the length od name should be between 5 and 30")
+   @NotEmpty(message = "Name cannot be null or empty")
    private String name;
 
+    @NotEmpty(message = "Email cannot be null or empty")
+    @Email(message = "Email should be a valid value")
     private String email;
 
+    @NotEmpty(message = "Mobilenumber cannot be null or empty")
+    @Pattern(regexp = "(^$|[0-9]{10})", message = "Number must be 10 digits")
     private String mobileNumber;
 
     private AccountsDto accountsDto;
